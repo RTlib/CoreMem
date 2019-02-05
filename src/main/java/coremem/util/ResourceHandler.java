4653421ef0d90dae14a3610f4eae41d87ff10b45
Ahmet Can Solak <asolak14@ku.edu.tr>
2019-02-04 16:36:59 -0800

    tests and implementation of copying a dll resource to a tempFile (#4)
    * tests and implementation of copying a dll resource to a tempFile
    
    * checking two files content same or not implemented manually, updated the points mentioned in the comments
    
    * unstable commit to migrate windowsenv for dev
    
    * fixed the load library code and test also updated the filename extraction part for OS compatibility
    
    * method names are changed with more generic ones to define functionality better

---
+++ src/main/java/coremem/util/ResourceHandler.java
@@ -0,0 +1,86 @@
+package coremem.util;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+
+public class ResourceHandler
+{
+
+    /**
+     * Simple wrapper function to compare contents of two files
+     */
+    public boolean twoFilesAreSame(File pCopied, File pRead)
+    {
+        // Check if both files exist
+        if (!pCopied.exists() || !pRead.exists())
+            return false;
+
+        // Both file's that are passed are to be of file type and not directory.
+        if (!pCopied.isFile() || !pRead.isFile())
+            return false;
+
+        if (pCopied.length() != pRead.length())
+            return false;
+
+        // Then compare the contents.
+        try (
+                InputStream is1 = new FileInputStream(pCopied);
+                InputStream is2 = new FileInputStream(pRead)
+        )
+        {
+            // Compare byte-by-byte - if performance issues occur solution is buffering
+            int data;
+            while ((data = is1.read()) != -1)
+                if (data != is2.read())
+                    return false;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    /**
+     * Method to copy a .dll resource from a jar
+     */
+    public File copyResourceFromJarToTempFile(String pRelativePathToFileInJar) throws IOException
+    {
+        String lFullFileName = new File(pRelativePathToFileInJar).getName();
+        int lIndex = lFullFileName.lastIndexOf('.');
+        String lFileName = lFullFileName.substring(0, lIndex);
+        String lFileExtension = lFullFileName.substring(lIndex);
+
+        File lFile = File.createTempFile(lFileName, lFileExtension);
+        lFile.deleteOnExit();
+
+        try (
+                InputStream an = getClass().getResourceAsStream(pRelativePathToFileInJar)
+        )
+        {
+            Files.copy(an, lFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lFile;
+    }
+
+    /**
+     * Method to load a .dll resource from a jar after copying into a TempFile
+     */
+    public File loadResourceFromJar(String pRelativePathToFileInJar) {
+        File lResultFile = null;
+        try {
+            lResultFile = this.copyResourceFromJarToTempFile(pRelativePathToFileInJar);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.load(lResultFile.getAbsolutePath());
+        return lResultFile;
+    }
+}
---
+++ src/test/java/coremem/util/test/ResourceHandlerTests.java
@@ -0,0 +1,85 @@
+package coremem.util.test;
+
+import static org.junit.Assert.*;
+
+import coremem.test.ClassScope;
+import coremem.util.ResourceHandler;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+
+/**
+ * ResourceHandler tests
+ *
+ * @author AhmetCanSolak
+ */
+public class ResourceHandlerTests
+{
+    /**
+     * Private instance of {@link ResourceHandler}
+     */
+    private ResourceHandler mRH = new ResourceHandler();
+
+    /**
+     * Test for checking if given two files has identically same content or not
+     */
+    @Test
+    public void testCopyAndLoadDLLFromJar()
+    {
+        File fCopied = null;
+        fCopied = mRH.loadResourceFromJar("/com/sun/jna/win32-x86-64/jnidispatch.dll");
+        System.out.println("For debug: " + fCopied.getAbsolutePath());
+
+        ClassLoader appLoader = ClassLoader.getSystemClassLoader();
+        ClassLoader currentLoader = ResourceHandlerTests.class.getClassLoader();
+
+        ClassLoader[] loaders = new ClassLoader[] { appLoader, currentLoader };
+        final String[] libraries = ClassScope.getLoadedLibraries(loaders);
+
+        // Check if it is loaded
+        assertTrue(Arrays.stream(libraries).anyMatch(fCopied.getAbsolutePath()::equals));
+
+    }
+
+    /**
+     * Test for copying file from loaded Jar to given destination
+     */
+    @Test
+    public void testDLLfromJar ()
+    {
+        // Copy the file from JAR
+        File fCopied = null;
+        try {
+            fCopied = mRH.copyResourceFromJarToTempFile("/com/sun/jna/win32-x86-64/jnidispatch.dll");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Get reference file
+        Path path = Paths.get("src/test/java/coremem/util/test/testartifact_jnidispatch.dll");
+        File fRead = new File(path.toAbsolutePath().toString());
+
+        // Check if they have same content
+        assertTrue(mRH.twoFilesAreSame(fCopied, fRead));
+    }
+
+    /**
+     * Test for checking if given two files has identically same content or not
+     */
+    @Test
+    public void testTwoFilesAreSame()
+    {
+        Path path = Paths.get("src/test/java/coremem/util/test/testartifact_jnidispatch.dll");
+        File fRead1 = new File(path.toAbsolutePath().toString());
+        File fRead2 = new File(path.toAbsolutePath().toString());
+
+        // Compare with two instance of same file
+        assertTrue(mRH.twoFilesAreSame(fRead1,fRead2));
+    }
+}
+
